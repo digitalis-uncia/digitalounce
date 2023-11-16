48a7f0b87e9b86d2e3ecabce578a044e4937c0e5
pbc-chain <114576940+pbc-chain@users.noreply.github.com>
2023-11-16 19:55:27 +0100

    Uncia valorem

--- src/validation.cpp
+++ src/validation.cpp
@@ -1271,7 +1271,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
         return 0;
 
     CAmount nSubsidy = 0 * COIN;
-    if (nHeight==1)nSubsidy = 75000000000 * COIN;else nSubsidy >>= halvings;
+	nSubsidy >>= halvings;
     return nSubsidy;
 }
 
